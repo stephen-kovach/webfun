2761eaf26158671094e8b41d8d7fa22ea78307c7
stephen-kovach <smkovach5458@gmail.com>
2022-04-25 21:12:00 -0500

    making connections

---
+++ w01d1_assignments/16_making_connections/script.js
@@ -0,0 +1,141 @@
+console.log("yo")
+
+console.log("Yo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function hide(element) {
+//     element.remove();
+// }
+
+
+// /* <div class="login">
+
+// <!-- JavaScript changing text -->
+// <button class="login-button" onclick="turnOff(this)">
+// Login</button>
+// </div> */
+
+
+// var count = 1
+// var countElement = document.querySelector("#count")
+
+// console.log(countElement);
+
+// function add1(){
+//     count++;
+//     countElement.innerText = "The count is " + count;
+//     console.log(count);
+// }
+
+
+function turnOff() {
+    newName.innerText = "Guess who?";
+    console.log("Guess Who?")
+}
+
+var CRCount = 2
+var YCCount = 418
+var countCRC = document.querySelector("#CRCount")
+var countYCC = document.querySelector("#YCCount")
+
+function hideTodd() {
+    byeTodd.remove();
+    CRCount--;
+    countCRC.innerText = CRCount;
+    console.log(CRCount);
+    YCCount++;
+    countYCC.innerText = YCCount;
+    console.log(YCCount);
+}
+
+function hidePhil() {
+    byePhil.remove();
+    CRCount--;
+    countCRC.innerText = CRCount;
+    console.log(CRCount);
+    YCCount++;
+    countYCC.innerText = YCCount;
+    console.log(YCCount);
+}
+
+
+// var CRCount = 1
+// var countCRC = document.querySelector("#CRCount")
+
+// function hideTodd() {
+//         byeTodd.remove();
+//         countCRC.innerText = CRCount;
+//         CRCount--;
+//         console.log(CRCount);
+//     }
+    
+//     function hidePhil() {
+//         byePhil.remove();
+//         countCRC.innerText = CRCount;
+//         CRCount--;
+//         console.log(CRCount);
+//     }
+    
+    
+    
+
+
+/* <div class="lower-right-box">
+
+
+/* <!-- JavaScript hiding definition button -->
+<button class="def-button" onclick='hide(this)'">Add Definiton</button>
+</div> */
+
+/* <div class="like-box">
+
+
+<!-- alert java -->
+<button class="like-button" onclick="alert('Ninja was liked')">13 likes</button>
+</div> */
+
+
+
+
+
+
+
+
+// var count = 1
+// var countElement = document.querySelector("#count")
+
+// console.log(countElement);
+
+// function add1(){
+//     count++;
+//     countElement.innerText = "The count is " + count;
+//     console.log(count);
+// }
+
+// function subtract1(){
+//     count--;
+//     countElement.innerText = "The count is " + count;
+//     console.log(count);
+// }
+
+
+// function hide(toddBox) {
+//     todd-box.remove();
+// }
+
+
+// function turnOff(element) {
+//     element.innerText = "Logout";
+// }
+
